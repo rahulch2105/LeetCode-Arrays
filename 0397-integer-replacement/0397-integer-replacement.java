2ab4c0c585422fdf442c92e5eca0f0b831b61f0a
RAHUL CH <rahulcch794@gmail.com>
2026-09-18 23:26:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (89.08%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int integerReplacement(int n) {
+
+        long num = n;
+        int count = 0;
+
+        while (num != 1) {
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            }
+
+            else {
+                if (num == 3 || num % 4 == 1) {
+                    num = num - 1;
+                }
+                else {
+                    num = num + 1;
+                }
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
